6c0e18f55bb746dd196456967e2076ee41cc9ae6
snuk182 <snuk188@gmail.com>
2018-05-22 23:00:08 +0200

    oop3 API almost there

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,19 @@ extern crate scintilla_sys;
 
 #[macro_use]
 extern crate lazy_static;
-//#[macro_use]
+#[macro_use]
 extern crate plygui_api;
 
 #[cfg(target_os="windows")]
 mod lib_win32;
-#[macro_use]
 #[cfg(target_os="windows")]
 extern crate plygui_win32;
 #[cfg(target_os="windows")]
 extern crate winapi;
 #[cfg(target_os="windows")]
 pub use lib_win32::Scintilla;
+#[cfg(target_os="windows")]
+pub use lib_win32::ScintillaWin32 as Spawner;
 
 #[cfg(target_os="macos")]
 mod lib_cocoa;
@@ -60,8 +61,28 @@ extern crate pango;
 #[cfg(feature = "gtk3")]
 pub use lib_gtk::Scintilla;
 
-pub const MEMBER_ID_SCINTILLA: &str = "Scintilla";
-
 pub trait UiScintilla: plygui_api::traits::UiControl {
     fn set_margin_width(&mut self, index: usize, width: isize);
+}
+
+pub mod development {
+	use super::*;
+	use plygui_api::development::*;
+	
+	pub trait ScintillaInner: ControlInner {
+		fn new() -> Box<UiScintilla>;
+		fn set_margin_width(&mut self, index: usize, width: isize);
+	}
+	
+	impl <T: ScintillaInner + Sized + 'static> UiScintilla for Member<Control<T>> {
+		fn set_margin_width(&mut self, index: usize, width: isize) {
+			self.as_inner_mut().as_inner_mut().set_margin_width(index, width)
+		}
+	}
+	// bloody fucking orphan rules
+	/*impl <T: ScintillaInner + Sized> Member<Control<T>> {
+		pub fn new() -> Box<UiScintilla> {
+			T::new()
+		}
+	} */
 }
