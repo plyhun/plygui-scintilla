47ce82bb6960b00949c0d3a97335a0503ab81fb3
admin <admin@192.168.128.129>
2018-10-09 23:02:09 +0200

    Cocoa console almost works

--- src/console/common.rs
+++ src/console/common.rs
@@ -113,6 +113,8 @@ impl ControlInner for ConsoleImpl {
             let window = self.root_mut().unwrap().as_any_mut().downcast_mut::<::plygui_gtk::prelude::imp::Window>().unwrap();
         	#[cfg(feature = "qt5")]
             let window = self.root_mut().unwrap().as_any_mut().downcast_mut::<::plygui_qt::prelude::imp::Window>().unwrap();
+            #[cfg(all(target_os = "macos", feature = "cocoa_"))]
+            let window = self.root_mut().unwrap().as_any_mut().downcast_mut::<::plygui_cocoa::prelude::imp::Window>().unwrap();
         	
         	window.as_inner_mut().as_inner_mut().as_inner_mut().on_frame((move |w: &mut ::plygui_api::controls::Window| {
         	    if let Some(console) = w.find_control_by_id_mut(my_id) {
