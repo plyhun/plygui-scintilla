cc691a4110d710b15e548c4421cff8a6a72a066f
admin <admin@192.168.192.130>
2020-03-25 22:10:26 +0100

    API update

--- src/imp/scintilla/mod.rs
+++ src/imp/scintilla/mod.rs
@@ -6,6 +6,8 @@ pub use mod_win32::{Scintilla, WindowsScintilla as ScintillaControl};
 
 #[cfg(all(target_os = "macos", feature = "cocoa_"))]
 pub(crate) mod mod_cocoa;
+#[cfg(all(target_os = "macos", feature = "cocoa_"))]
+pub use mod_cocoa::{Scintilla, CocoaScintilla as ScintillaControl};
 
 #[cfg(feature = "qt5")]
 pub(crate) mod mod_qt;
